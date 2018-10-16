89802f2def3929e2663e07c37815ecb7a56bbda2
Yvoox <alexandre-colicchio@hotmail.fr>
2018-10-16 15:02:29 +0200

    Added Standardize function into helpers & Data wrangling into implementations

--- implementations.py
+++ implementations.py
@@ -2,6 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from template.helpers import *
 
-print('Hello, World!')
+
+pathTrain = "./template/train.csv"
+pathTest = "./template/test.csv"
+
+
+yb,input_data,ids = load_csv_data(pathTrain)
+#yb -> y vector : it's the real output
+#input_data -> x parameters
+#ids -> id of each data row
+
+input_data = standardize(input_data)
+
+random_w = np.random.random(len(input_data[0])+1)
+gamma = 0.7
+max_iters = 50
+
+tx = np.c_[np.ones(len(yb)), input_data]
+
+
 
 input()
