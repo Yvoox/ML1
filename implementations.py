217e0446e273a0260a9965a837e76cff8da7f19c
Yvoox <alexandre-colicchio@hotmail.fr>
2018-10-16 16:34:01 +0200

    Added least squares implementaiton & test it

--- implementations.py
+++ implementations.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from template.helpers import *
 from least_squares_SGD import *
+from least_squares import *
+
 
 
 pathTrain = "./template/train.csv"
@@ -21,11 +23,16 @@ max_iters = 50
 
 tx = np.c_[np.ones(len(yb)), input_data]
 
-
-wSGD,lossSGD = least_squares_SGD(yb,tx,random_w,max_iters,gamma)
+#LEAST SQUARES SGD TEST
+#wSGD,lossSGD = least_squares_SGD(yb,tx,random_w,max_iters,gamma)
 #s = 'SGD : w : ' + repr(wSGD) + ' - Loss: ' + repr(lossSGD)
 #print(s)
-y_pred = predict_labels(wSGD,tx)
+#y_pred = predict_labels(wSGD,tx)
+#percent = comparePredict(yb,y_pred)
+#print(repr(percent))
+
+wLS = least_squares(yb,tx)
+y_pred = predict_labels(wLS,tx)
 percent = comparePredict(yb,y_pred)
 print(repr(percent))
 
--- least_squares.py
+++ least_squares.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-print('Hello, World!')
-
-input()
+def least_squares(y, tx):
+    a = tx.T.dot(tx)
+    b = tx.T.dot(y)
+    return np.linalg.solve(a, b)
